4646c1fd92547ab3c69abc0c86b65694ee91ad5b
XKESAR <karimmasri52@gmail.com>
2023-01-14 20:27:45 +0200

    students display

--- src/App.js
+++ src/App.js
@@ -2,24 +2,33 @@ import Auth from "./components/Auth/Auth";
 import AdminNavBar from "./admin/components/AdminNavBar";
 import TeacherNavBar from "./teacher/components/TeacherNavBar";
 import Student from "./student";
+import Display from "./admin/components/Display";
+
 import "./App.css";
 
 function App() {
   // const token = false;
   const token = true;
-  // const role = "admin";
+  const role = "admin";
   // const role = "teacher";
-  const role = "student";
+  // const role = "student";
   return (
     <div>
       {!token ? (
         <Auth />
       ) : role === "admin" ? (
-        <AdminNavBar />
+        <>
+          <AdminNavBar />
+          <Display />
+        </>
       ) : role === "teacher" ? (
-        <TeacherNavBar />
+        <>
+          <TeacherNavBar />
+        </>
       ) : role === "student" ? (
-        <Student />
+        <>
+          <Student />
+        </>
       ) : null}
     </div>
   );
